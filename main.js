72afebf1b366cf4d3f509f972a20fd309dad1fa1
roogue <57899698+roogue@users.noreply.github.com>
2022-01-31 20:29:13 +0800

    Remove compiled files

--- main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Rlyrics = void 0;
-var rlyrics_1 = require("./src/bin/rlyrics");
-Object.defineProperty(exports, "Rlyrics", { enumerable: true, get: function () { return rlyrics_1.Rlyrics; } });
